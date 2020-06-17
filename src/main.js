2c1cc512d9441b5be2f3d41399c2a98436509c48
Marcin Koziej <marcin@cahoots.pl>
2020-06-17 10:54:05 +0200

    add Sentry middleware

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import {crypto, service, config} from 'proca_cli'
 import * as Sentry from '@sentry/node';
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({ dsn: process.env.SENTRY_DSN });
+
+function sentryHandler(lambdaHandler) {
+  return async (event, context) => {
+    try {
+      return await lambdaHandler(event, context);
+    } catch (error) {
+      Sentry.captureException(error);
+      await Sentry.flush(2000);
+      throw error;
+    }
+  };
 }
 
 // Handler
-exports.handler = async function(event, context) {
+async function identitySync(event, context) {
   const sync_all = event.Records.map(record => {
     let action = JSON.parse(record.body)
     action = crypto.decryptAction(action, config)
@@ -23,4 +32,13 @@ exports.handler = async function(event, context) {
   })
 
   return Promise.all(sync_all)
+}
+
+
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
+  exports.handler = sentryHandler(identitySync)
+} else {
+  exports.handler = identitySync
 }
